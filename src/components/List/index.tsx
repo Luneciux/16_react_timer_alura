5091d3bbc15f6ca3d2ae05ff7a3b086c2ff8ddfe
josue.costa <jejosu346@gmail.com>
2023-01-20 22:49:58 -0300

    Test on function and class components

---
+++ src/components/List/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function List () {
+    const tasks = [{
+        task: 'Test',
+        time: '02:00::00'
+    }, {
+        task: 'Javascript',
+        time: '01:00::00'
+    }, {
+        task: 'Typescript',
+        time: '01:00:00'
+    }]
+
+    return (
+        <aside>
+            <h2>Daily Tasks</h2>
+            <ul>
+                {tasks.map((item, index) => (
+                    <li key={index}>
+                        <h3>{item.task}</h3>
+                        <span>{item.time}</span>    
+                    </li>
+                ))}
+            </ul>
+        </aside>
+    )
+}
+
+export default List;
