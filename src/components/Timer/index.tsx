7e95b33ddd82487747b0e7055a891a3867df82da
josue.costa <jejosu346@gmail.com>
2023-01-23 22:27:00 -0300

    reception of a task in props and new time state

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,9 +1,24 @@
+import { ITask } from "../../types/ITask";
+import { useState } from "react";
 import Button from "../Button";
 import Clock from "./Clock";
 
+import calcTime from "../../common/utils/date";
+
 import style from './Timer.module.scss';
 
-function Timer () {
+interface TimerProps {
+    selected: ITask | undefined,
+}
+
+function Timer ({selected} : TimerProps) {
+
+    const [time, setTime] = useState<number>();
+
+    if(selected?.time){
+        setTime(calcTime(selected?.time));
+    }
+    
     return (
         <div className={style.timer}>
             <p className={style.title}>Escolha um card e inicie a contagem</p>
